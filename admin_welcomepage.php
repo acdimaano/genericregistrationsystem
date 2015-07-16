cf9d8759f1d4ae3196bdf7aa309fba3ddb46655e
acdimaano <alan@mirzabros.com>
2015-07-17 11:33:09 +1200

    alan test
    alan tetst

--- admin_welcomepage.php
+++ admin_welcomepage.php
@@ -25,7 +25,7 @@
 </head>
 
 <body>
-    <!-- Defining the page layout for admin -->
+    <!-- Defining the page layout -->
     <div class="page-layout">
     <div class="header page-title background-header">
             <div class="background-image" style="background-image:url(images/admin_header.jpg)";></div>
